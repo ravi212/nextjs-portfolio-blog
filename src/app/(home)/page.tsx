c2dcd71a58f3977356592ba83676b1d01579428c
ravi212 <raviraina212@gmail.com>
2024-10-16 21:36:31 +0530

    Cache issue fix and deleted unused assets

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,8 +1,11 @@
 import HomeSection from "@/components/molecules/client/home";
 import { getAllProjects } from "@/lib/actions/project.action";
+import { revalidatePath } from "next/cache";
 
 export default async function Page() {
   const projects = (await getAllProjects())?.projects;
+  
+  revalidatePath('/(home)', 'page');
 
   return (
     <section>
